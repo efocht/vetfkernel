cc954b21ddaa7b552f4cb15ad7597a2d48da251e
Kazuhisa Ishizaka <k-ishizaka@ay.jp.nec.com>
2018-09-21 18:20:47 +0900

    Add Div, Neg and Sum

--- src/ops.cc
+++ src/ops.cc
@@ -9,7 +9,6 @@
 
 #include "vednn.h"
 
-REGISTER_KERNEL("Snapshot", "op_Snapshot")
 REGISTER_KERNEL("Fill", "op_fill");
 REGISTER_KERNEL("AddN", "op_AddN");
 REGISTER_KERNEL("BiasAdd", "op_BiasAdd");
@@ -17,6 +16,10 @@ REGISTER_KERNEL("BiasAddGrad", "op_BiasAddGrad");
 REGISTER_KERNEL("Relu", "op_Relu");
 REGISTER_KERNEL("ReluGrad", "op_ReluGrad");
 REGISTER_KERNEL("Mul", "op_Mul");
+REGISTER_KERNEL("Snapshot", "op_Snapshot")
+REGISTER_KERNEL("Div", "op_Div");
+REGISTER_KERNEL("Neg", "op_Neg");
+REGISTER_KERNEL("Sum", "op_Sum");
 
 #define CHECK_ARG_LEN(l0, l1) \
   if ((l0) != (l1)) { \
@@ -33,6 +36,9 @@ extern "C" {
   int op_ReluGrad(const void* arg, size_t len);
   int op_Mul(const void* arg, size_t len);
   int op_Snapshot(const void* arg, size_t len);
+  int op_Div(const void* arg, size_t len);
+  int op_Neg(const void* arg, size_t len);
+  int op_Sum(const void* arg, size_t len);
 }
 
 namespace {
@@ -478,8 +484,193 @@ int op_Snapshot(const void* arg, size_t len)
   memcpy(reinterpret_cast<void*>(p->dst),
          reinterpret_cast<const void*>(p->src),
          p->size);
-  LOG(2) << __FUNCTION__ << " done";
+  LOG(2) << __FUNCTION__ << ": done";
 
   return 0;
 }
 
+//
+// Div
+// 
+
+namespace {
+template <typename T>
+void div_nx1(uint64_t out, uint64_t in0, uint64_t in1, size_t nelems)
+{
+  T* po = reinterpret_cast<T*>(out);
+  const T* pi0 = reinterpret_cast<const T*>(in0);
+  T i1 = *reinterpret_cast<const T*>(in1);
+
+  for (size_t i = 0; i < nelems; ++i) {
+    po[i] = pi0[i] / i1;
+  }
+}
+
+// nelems_in0 > nelems_in1
+template <typename T>
+void div2_nn_n1(uint64_t out, 
+                uint64_t in0,
+                uint64_t in1, 
+                size_t n0,
+                size_t n1)
+{
+  T* po = reinterpret_cast<T*>(out);
+  const T* pi0 = reinterpret_cast<const T*>(in0);
+  const T* pi1 = reinterpret_cast<const T*>(in1);
+
+  for (size_t i = 0; i < n0; ++i) {
+    for (size_t j = 0; j < n1; ++j) {
+      po[i * n1 + j] = pi0[i * n1 + j] / pi1[i];
+    }
+  }
+
+}
+} // namespace
+
+int op_Div(const void* args, size_t len)
+{
+  LOG(2) << __FUNCTION__ << ": begin";
+  struct Args {
+    int dtype;
+    uint64_t in0;
+    uint64_t in1;
+    uint64_t out;
+    int32_t dims_in0;
+    int32_t dims_in1;
+    int32_t dims_out;
+    int64_t nelems_in0;
+    int64_t nelems_in1;
+    int64_t nelems_out;
+    int64_t dim_size_in0[8];
+    int64_t dim_size_in1[8];
+    int64_t dim_size_out[8];
+  } const* p;
+
+  CHECK_ARG_LEN(len, sizeof(Args));
+  p = reinterpret_cast<const Args*>(args);
+
+  LOG(3) << "op_Div:"
+    << " dims_in0=" << p->dims_in0
+    << " dims_in1=" << p->dims_in1
+    << " dims_out=" << p->dims_out
+    << " nelems_in0=" << p->nelems_in0
+    << " nelems_in1=" << p->nelems_in1
+    << " nelems_out=" << p->nelems_out;
+
+  int ret = 0;
+  if (p->dtype == DT_FLOAT) {
+    if (p->nelems_in0 == 1) {
+      assert(p->nelems_in1 == p->nelems_out);
+      div_nx1<float>(p->out, p->in1, p->in0, p->nelems_out);
+    } else if (p->nelems_in1 == 1) {
+      assert(p->nelems_in0 == p->nelems_out);
+      div_nx1<float>(p->out, p->in0, p->in1, p->nelems_out);
+    } else if (p->dims_in0 == 2 && p->dims_in1 == 2) {
+      if (p->dim_size_in0[0] == p->dim_size_in1[0]
+          && p->dim_size_in1[1] == 1) {
+        div2_nn_n1<float>(p->out, p->in0, p->in1, p->dim_size_in0[0], p->dim_size_in0[1]);
+      }
+    } else {
+      ret = 1;
+    }
+
+  }
+
+  LOG(2) << __FUNCTION__ << ": end. ret=" << ret;
+
+  return ret;
+}
+
+namespace {
+template<typename Tin, typename Tout>
+  void neg(uint64_t out, uint64_t in, size_t nelems)
+  {
+    Tout* po = reinterpret_cast<Tout*>(out);
+    const Tin* pi = reinterpret_cast<Tin*>(in);
+
+    for (int64_t i = 0; i < nelems; ++i) {
+      po[i] = - pi[i];
+    }
+  }
+}
+
+int op_Neg(const void* args, size_t len)
+{
+  LOG(2) << __FUNCTION__ << " begin";
+
+  struct _Tensor {
+    int dtype;
+    int data_format;
+    uint64_t addr;
+    int32_t dims;
+    int64_t nelems;
+    int64_t dim_size[8];
+  };
+
+  struct Args {
+    _Tensor in;
+    _Tensor out;
+  } const* p;
+
+  CHECK_ARG_LEN(len, sizeof(Args));
+  p = reinterpret_cast<const Args*>(args);
+
+  if (p->in.dtype == DT_FLOAT || p->out.dtype == DT_FLOAT) {
+    neg<float, float>(p->out.addr, p->in.addr, p->in.nelems);
+  } else {
+    return 1;
+  }
+
+  LOG(2) << __FUNCTION__ << " end";
+  return 0;
+}
+
+namespace {
+template <typename T>
+int reduction_d2a1(uint64_t out, uint64_t in, size_t dim0, size_t dim1)
+{
+  T* po = reinterpret_cast<T*>(out);
+  const T* pi = reinterpret_cast<const T*>(in);
+
+  for (size_t i = 0; i < dim0; ++i) {
+    T s = T(0);
+    for (size_t j = 0; j < dim1; ++j) {
+      s += pi[i * dim1 + j];
+    }
+    po[i] = s;
+  }
+
+  return 0;
+}
+}
+
+int op_Sum(const void* args, size_t len)
+{
+  LOG(2) << __FUNCTION__ << " begin";
+
+  struct Args {
+    int dtype;
+    int ndims;
+    uint64_t in;
+    uint64_t out;
+    int64_t dim_size[2];
+    int axis;
+  } const* p;
+
+  CHECK_ARG_LEN(len, sizeof(Args));
+  p = reinterpret_cast<const Args*>(args);
+
+  int ret = 1;
+
+  LOG(3) << __FUNCTION__ << ": ndims=" << p->ndims << " axis=" << p->axis;
+
+  if (p->dtype == DT_FLOAT) {
+    if (p->ndims == 2 && p->axis == 1) {
+      ret = reduction_d2a1<float>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
+    }
+  }
+
+
+  LOG(2) << __FUNCTION__ << " end. ret=" << ret;
+  return ret;
+}
