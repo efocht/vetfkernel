951d48938eea8f392369140d9aff60381ee567b1
Yoshiyuki Ohno <y-ohno@ji.jp.nec.com>
2018-11-02 16:53:52 +0900

    split reduction_ops

--- src/ops.cc
+++ src/ops.cc
@@ -47,8 +47,6 @@ REGISTER_KERNEL("BiasAddGrad", "op_BiasAddGrad");
 REGISTER_KERNEL("Relu", "op_Relu");
 REGISTER_KERNEL("ReluGrad", "op_ReluGrad");
 REGISTER_KERNEL("Snapshot", "op_Snapshot")
-REGISTER_KERNEL("Sum", "op_Sum");
-REGISTER_KERNEL("Prod", "op_Prod");
 REGISTER_KERNEL("Transpose", "op_Transpose");
 REGISTER_KERNEL("MatMul", "op_MatMul");
 REGISTER_KERNEL("Softmax", "op_Softmax");
@@ -75,8 +73,6 @@ extern "C" {
   int op_Relu(const void* arg, size_t len);
   int op_ReluGrad(const void* arg, size_t len);
   int op_Snapshot(const void* arg, size_t len);
-  int op_Sum(const void* arg, size_t len);
-  int op_Prod(const void* arg, size_t len);
   int op_Neg(const void* arg, size_t len);
   int op_Floor(const void* arg, size_t len);
   int op_Transpose(const void* arg, size_t len);
@@ -567,129 +563,6 @@ int op_Neg(const void* args, size_t len)
 
   LOG(2) << __FUNCTION__ << " end";
   return 0;
-}
-
-//
-// Sum
-//
-
-namespace {
-template <typename T>
-int sum_d2a1(uint64_t out, uint64_t in, size_t dim0, size_t dim1)
-{
-  T* po = reinterpret_cast<T*>(out);
-  const T* pi = reinterpret_cast<const T*>(in);
-
-  for (size_t i = 0; i < dim0; ++i) {
-    T s = T(0);
-    for (size_t j = 0; j < dim1; ++j) {
-      s += pi[i * dim1 + j];
-    }
-    po[i] = s;
-  }
-
-  return 0;
-}
-}
-
-int op_Sum(const void* args, size_t len)
-{
-  LOG(2) << __FUNCTION__ << " begin";
-
-  struct Args {
-    int dtype;
-    int ndims;
-    uint64_t in;
-    uint64_t out;
-    int64_t dim_size[2];
-    int axis;
-  } const* p;
-
-  CHECK_ARG_LEN(len, sizeof(Args));
-  p = reinterpret_cast<const Args*>(args);
-
-  int ret = 1;
-
-  LOG(3) << __FUNCTION__ << ": ndims=" << p->ndims << " axis=" << p->axis;
-
-  if (p->dtype == DT_FLOAT) {
-    if (p->ndims == 2 && p->axis == 1) {
-      ret = sum_d2a1<float>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
-    }
-  }
-  else if (p->dtype == DT_DOUBLE) {
-    if (p->ndims == 2 && p->axis == 1) {
-      ret = sum_d2a1<double>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
-    }
-  }
-
-
-  LOG(2) << __FUNCTION__ << " end. ret=" << ret;
-  return ret;
-}
-
-//
-// Prod
-//
-
-namespace {
-template <typename T>
-int prod_d2a1(uint64_t out, uint64_t in, size_t dim0, size_t dim1)
-{
-  T* po = reinterpret_cast<T*>(out);
-  const T* pi = reinterpret_cast<const T*>(in);
-
-  for (size_t i = 0; i < dim0; ++i) {
-    T s = T(1);
-    for (size_t j = 0; j < dim1; ++j) {
-      s *= pi[i * dim1 + j];
-    }
-    po[i] = s;
-  }
-
-  return 0;
-}
-}
-
-int op_Prod(const void* args, size_t len)
-{
-  LOG(2) << __FUNCTION__ << " begin";
-
-  struct Args {
-    int dtype;
-    int ndims;
-    uint64_t in;
-    uint64_t out;
-    int64_t dim_size[2];
-    int axis;
-  } const* p;
-
-  CHECK_ARG_LEN(len, sizeof(Args));
-  p = reinterpret_cast<const Args*>(args);
-
-  int ret = 1;
-
-  LOG(3) << __FUNCTION__ << ": ndims=" << p->ndims << " axis=" << p->axis;
-
-  if (p->dtype == DT_FLOAT) {
-    if (p->ndims == 2 && p->axis == 1) {
-      ret = prod_d2a1<float>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
-    }
-  }
-  else if (p->dtype == DT_DOUBLE) {
-    if (p->ndims == 2 && p->axis == 1) {
-      ret = prod_d2a1<double>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
-    }
-  }
-  else if (p->dtype == DT_INT32) {
-    if (p->ndims == 2 && p->axis == 1) {
-      ret = prod_d2a1<int32_t>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
-    }
-  }
-
-
-  LOG(2) << __FUNCTION__ << " end. ret=" << ret;
-  return ret;
 }
 
 //
