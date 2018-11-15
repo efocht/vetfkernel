c0778133bcbe0de2d9af71f2b732aeac6c7dfbe0
cdmalon <malon@nec-labs.com>
2018-11-16 07:25:11 +0900

    reduction_ops.cc - cover more cases

--- src/reduction_ops.cc
+++ src/reduction_ops.cc
@@ -45,6 +45,65 @@ int sum_d2a1(uint64_t out, uint64_t in, size_t dim0, size_t dim1)
 
   return 0;
 }
+
+template <typename T>
+int sum_d2a0(uint64_t out, uint64_t in, size_t dim0, size_t dim1)
+{
+  T* po = reinterpret_cast<T*>(out);
+  const T* pi = reinterpret_cast<const T*>(in);
+
+  for (size_t j = 0; j < dim1; ++j) {
+    T s = T(0);
+    for (size_t i = 0; i < dim0; ++i) {
+      s += pi[i * dim1 + j];
+    }
+    po[j] = s;
+  }
+
+  return 0;
+}
+
+template <typename T>
+int sum_d3a1(uint64_t out, uint64_t in, size_t dim0, size_t dim1, size_t dim2)
+{
+  T* po = reinterpret_cast<T*>(out);
+  const T* pi = reinterpret_cast<const T*>(in);
+
+  size_t dim12 = dim1 * dim2;
+
+  for (size_t i = 0; i < dim0; ++i) {
+    for (size_t k = 0; k < dim2; ++k) {
+      T s = T(0);
+      for (size_t j = 0; j < dim1; ++j) {
+        s += pi[i * dim12 + j * dim2 + k];
+      }
+      po[i * dim2 + k] = s;
+    }
+  }
+
+  return 0;
+}
+
+template <typename T>
+int sum_d3a02(uint64_t out, uint64_t in, size_t dim0, size_t dim1, size_t dim2)
+{
+  T* po = reinterpret_cast<T*>(out);
+  const T* pi = reinterpret_cast<const T*>(in);
+
+  size_t dim12 = dim1 * dim2;
+
+  for (size_t j = 0; j < dim1; ++j) {
+    T s = T(0);
+    for (size_t i = 0; i < dim0; ++i) {
+      for (size_t k = 0; k < dim2; ++k) {
+        s += pi[i * dim12 + j * dim2 + k];
+      }
+    }
+    po[j] = s;
+  }
+
+  return 0;
+}
 }
 
 int op_Sum(const void* args, size_t len)
@@ -56,7 +115,7 @@ int op_Sum(const void* args, size_t len)
     int ndims;
     uint64_t in;
     uint64_t out;
-    int64_t dim_size[2];
+    int64_t dim_size[3];
     int axis;
   } const* p;
 
@@ -71,10 +130,28 @@ int op_Sum(const void* args, size_t len)
     if (p->ndims == 2 && p->axis == 1) {
       ret = sum_d2a1<float>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
     }
+    if (p->ndims == 2 && p->axis == 0) {
+      ret = sum_d2a0<float>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
+    }
+    if (p->ndims == 3 && p->axis == 1) {
+      ret = sum_d3a1<float>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
+    }
+    if (p->ndims == 3 && p->axis == 0) {
+      ret = sum_d3a02<float>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
+    }
   }
   else if (p->dtype == DT_DOUBLE) {
     if (p->ndims == 2 && p->axis == 1) {
       ret = sum_d2a1<double>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
+    }
+    if (p->ndims == 2 && p->axis == 0) {
+      ret = sum_d2a0<double>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
+    }
+    if (p->ndims == 3 && p->axis == 1) {
+      ret = sum_d3a1<double>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
+    }
+    if (p->ndims == 3 && p->axis == 0) {
+      ret = sum_d3a02<double>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
     }
   }
 
@@ -104,6 +181,65 @@ int prod_d2a1(uint64_t out, uint64_t in, size_t dim0, size_t dim1)
 
   return 0;
 }
+
+template <typename T>
+int prod_d2a0(uint64_t out, uint64_t in, size_t dim0, size_t dim1)
+{
+  T* po = reinterpret_cast<T*>(out);
+  const T* pi = reinterpret_cast<const T*>(in);
+
+  for (size_t j = 0; j < dim1; ++j) {
+    T s = T(1);
+    for (size_t i = 0; i < dim0; ++i) {
+      s *= pi[i * dim1 + j];
+    }
+    po[j] = s;
+  }
+
+  return 0;
+}
+
+template <typename T>
+int prod_d3a1(uint64_t out, uint64_t in, size_t dim0, size_t dim1, size_t dim2)
+{
+  T* po = reinterpret_cast<T*>(out);
+  const T* pi = reinterpret_cast<const T*>(in);
+
+  size_t dim12 = dim1 * dim2;
+
+  for (size_t i = 0; i < dim0; ++i) {
+    for (size_t k = 0; k < dim2; ++k) {
+      T s = T(1);
+      for (size_t j = 0; j < dim1; ++j) {
+        s *= pi[i * dim12 + j * dim2 + k];
+      }
+      po[i * dim2 + k] = s;
+    }
+  }
+
+  return 0;
+}
+
+template <typename T>
+int prod_d3a02(uint64_t out, uint64_t in, size_t dim0, size_t dim1, size_t dim2)
+{
+  T* po = reinterpret_cast<T*>(out);
+  const T* pi = reinterpret_cast<const T*>(in);
+
+  size_t dim12 = dim1 * dim2;
+
+  for (size_t j = 0; j < dim1; ++j) {
+    T s = T(1);
+    for (size_t i = 0; i < dim0; ++i) {
+      for (size_t k = 0; k < dim2; ++k) {
+        s *= pi[i * dim12 + j * dim2 + k];
+      }
+    }
+    po[j] = s;
+  }
+
+  return 0;
+}
 }
 
 int op_Prod(const void* args, size_t len)
@@ -115,7 +251,7 @@ int op_Prod(const void* args, size_t len)
     int ndims;
     uint64_t in;
     uint64_t out;
-    int64_t dim_size[2];
+    int64_t dim_size[3];
     int axis;
   } const* p;
 
@@ -130,15 +266,42 @@ int op_Prod(const void* args, size_t len)
     if (p->ndims == 2 && p->axis == 1) {
       ret = prod_d2a1<float>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
     }
+    if (p->ndims == 2 && p->axis == 0) {
+      ret = prod_d2a0<float>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
+    }
+    if (p->ndims == 3 && p->axis == 1) {
+      ret = prod_d3a1<float>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
+    }
+    if (p->ndims == 3 && p->axis == 0) {
+      ret = prod_d3a02<float>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
+    }
   }
   else if (p->dtype == DT_DOUBLE) {
     if (p->ndims == 2 && p->axis == 1) {
       ret = prod_d2a1<double>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
     }
+    if (p->ndims == 2 && p->axis == 0) {
+      ret = prod_d2a0<double>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
+    }
+    if (p->ndims == 3 && p->axis == 1) {
+      ret = prod_d3a1<double>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
+    }
+    if (p->ndims == 3 && p->axis == 0) {
+      ret = prod_d3a02<double>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
+    }
   }
   else if (p->dtype == DT_INT32) {
     if (p->ndims == 2 && p->axis == 1) {
       ret = prod_d2a1<int32_t>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
+    }
+    if (p->ndims == 2 && p->axis == 0) {
+      ret = prod_d2a0<int32_t>(p->out, p->in, p->dim_size[0], p->dim_size[1]);
+    }
+    if (p->ndims == 3 && p->axis == 1) {
+      ret = prod_d3a1<int32_t>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
+    }
+    if (p->ndims == 3 && p->axis == 0) {
+      ret = prod_d3a02<int32_t>(p->out, p->in, p->dim_size[0], p->dim_size[1], p->dim_size[2]);
     }
   }
 
