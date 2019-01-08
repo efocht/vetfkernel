51f88aeeb1e335179b5cfe6d2463be6707390077
Yoshiyuki Ohno <y-ohno@ji.jp.nec.com>
2019-01-08 09:36:32 +0900

    add ApplyAdam

---
+++ src/dense_update_functor.cc
@@ -0,0 +1,72 @@
+#include <cstdio>
+#include <cstdint>
+#include <cstdint>
+#include <cassert>
+#include <algorithm>
+#include "kernel.h"
+#include "types.h"
+#include "log.h"
+
+#include <omp.h>
+
+REGISTER_KERNEL("DenseUpdateAssign", "op_DenseUpdateAssign");
+
+#define CHECK_ARG_LEN(l0, l1) \
+  if ((l0) != (l1)) { \
+      fprintf(stderr, "%s: illegal argument length: %ld expected but %ld\n", (l1), (l0)); \
+      return 1; \
+  }
+
+extern "C" {
+  int op_DenseUpdateAssign(const void* arg, size_t len);
+}
+
+
+//
+// DenseUpdateAssign
+//
+
+namespace {
+
+template <typename T>
+int dense_update_assign(int64_t num_elements,
+                    uint64_t dst_ptr, uint64_t src_ptr )
+{
+  T* dst = reinterpret_cast<T*>(dst_ptr);
+  const T* src = reinterpret_cast<const T*>(src_ptr);
+
+  for(int64_t i=0; i<num_elements; i++) {
+    dst[i] = src[i] ;
+  }
+  return 0 ;
+}
+}
+
+int op_DenseUpdateAssign(const void* args, size_t len)
+{
+  LOG(2) << __FUNCTION__ << " begin";
+
+  struct Args {
+    int dtype;
+    int64_t num_elements ;
+    uint64_t dst_ptr, src_ptr ;
+  } const* p;
+
+  CHECK_ARG_LEN(len, sizeof(Args));
+  p = reinterpret_cast<const Args*>(args);
+
+  int ret = 1;
+
+  if (p->dtype == DT_FLOAT) {
+    ret = dense_update_assign<float> (p->num_elements,
+                                      p->dst_ptr, p->src_ptr) ;
+  }
+  else if (p->dtype == DT_DOUBLE) {
+    ret = dense_update_assign<double>(p->num_elements,
+                                      p->dst_ptr, p->src_ptr) ;
+  }
+
+  LOG(2) << __FUNCTION__ << " end. ret=" << ret;
+  return ret;
+}
+
---
+++ src/training_ops.cc
@@ -0,0 +1,113 @@
+#include <cstdio>
+#include <cstdint>
+#include <cstdint>
+#include <cassert>
+#include <algorithm>
+#include "kernel.h"
+#include "types.h"
+#include "log.h"
+
+#include <omp.h>
+
+REGISTER_KERNEL("ApplyAdam", "op_ApplyAdam");
+
+#define CHECK_ARG_LEN(l0, l1) \
+  if ((l0) != (l1)) { \
+      fprintf(stderr, "%s: illegal argument length: %ld expected but %ld\n", (l1), (l0)); \
+      return 1; \
+  }
+
+extern "C" {
+  int op_ApplyAdam(const void* arg, size_t len);
+}
+
+
+//
+// ApplyAdam
+//
+
+namespace {
+
+template <typename T>
+int apply_adam(bool use_nesterov, int64_t num_elements,
+               uint64_t var_ptr, uint64_t m_ptr, uint64_t v_ptr,
+               uint64_t beta1_power_ptr, uint64_t beta2_power_ptr,
+               uint64_t lr_ptr,
+               uint64_t beta1_ptr, uint64_t beta2_ptr, uint64_t epsilon_ptr,
+               uint64_t grd_ptr )
+{
+  T* var = reinterpret_cast<T*>(var_ptr);
+  T* m   = reinterpret_cast<T*>(m_ptr);
+  T* v   = reinterpret_cast<T*>(v_ptr);
+
+  const T* grd = reinterpret_cast<const T*>(grd);
+
+  const T beta1_power = reinterpret_cast<const T*>(beta1_power_ptr)[0];
+  const T beta2_power = reinterpret_cast<const T*>(beta2_power_ptr)[0];
+  const T lr = reinterpret_cast<const T*>(lr_ptr)[0];
+  const T beta1 = reinterpret_cast<const T*>(beta1_ptr)[0];
+  const T beta2 = reinterpret_cast<const T*>(beta2_ptr)[0];
+  const T epsilon = reinterpret_cast<const T*>(epsilon_ptr)[0];
+
+  const T one = T(1.) ; 
+
+  for(int64_t i=0; i<num_elements; i++) {
+    m[i] = m[i] + (one - beta1) * (grd[i] - m[i]) ;
+  }
+  for(int64_t i=0; i<num_elements; i++) {
+    v[i] = v[i] + (one - beta2) * (grd[i]*grd[i] - v[i]) ;
+  }
+  if( use_nesterov ) {
+
+  }
+  else {
+    const T k = (lr * ( one - beta2_power) * (one - beta2_power)  / ( one - beta1_power)) ;
+    for(int64_t i=0; i<num_elements; i++) {
+      var[i] -= k * m[i] / (epsilon + std::sqrt(v[i])) ;
+    }
+  }
+
+  return 0 ;
+}
+}
+
+int op_ApplyAdam(const void* args, size_t len)
+{
+  LOG(2) << __FUNCTION__ << " begin";
+
+  struct Args {
+    int dtype;
+    bool use_nesterov_ ;
+    int64_t num_elements ;
+    uint64_t var_ptr, m_ptr, v_ptr ;
+    uint64_t beta1_power_ptr, beta2_power_ptr ;
+    uint64_t lr ;
+    uint64_t beta1_ptr, beta2_ptr, epsilon_ptr ;
+    uint64_t grad_ptr;
+  } const* p;
+
+  CHECK_ARG_LEN(len, sizeof(Args));
+  p = reinterpret_cast<const Args*>(args);
+
+  int ret = 1;
+
+  if (p->dtype == DT_FLOAT) {
+    ret = apply_adam<float> (p->use_nesterov_, p->num_elements,
+                             p->var_ptr, p->m_ptr, p->v_ptr,
+                             p->beta1_power_ptr, p->beta2_power_ptr, p->lr,
+                             p->beta1_ptr, p->beta2_ptr, p->epsilon_ptr,
+                             p->grad_ptr ) ;
+  }
+  else if (p->dtype == DT_DOUBLE) {
+    ret = apply_adam<double>(p->use_nesterov_, p->num_elements,
+                             p->var_ptr, p->m_ptr, p->v_ptr,
+                             p->beta1_power_ptr, p->beta2_power_ptr, p->lr,
+                             p->beta1_ptr, p->beta2_ptr, p->epsilon_ptr,
+                             p->grad_ptr ) ;
+  }
+
+
+  LOG(2) << __FUNCTION__ << " end. ret=" << ret;
+  return ret;
+}
+
