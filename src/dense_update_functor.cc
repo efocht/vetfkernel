695dd3bcf380d8cadafd8a6a2475dbf129821e5c
Yoshiyuki Ohno <y-ohno@ji.jp.nec.com>
2019-01-16 13:32:17 +0900

    add DenseUpdateAssignAdd, DenseUpdateAssignSub

--- src/dense_update_functor.cc
+++ src/dense_update_functor.cc
@@ -10,6 +10,8 @@
 #include <omp.h>
 
 REGISTER_KERNEL("DenseUpdateAssign", "op_DenseUpdateAssign");
+REGISTER_KERNEL("DenseUpdateAssignAdd", "op_DenseUpdateAssignAdd");
+REGISTER_KERNEL("DenseUpdateAssignSub", "op_DenseUpdateAssignSub");
 
 #define CHECK_ARG_LEN(l0, l1) \
   if ((l0) != (l1)) { \
@@ -19,8 +21,9 @@ REGISTER_KERNEL("DenseUpdateAssign", "op_DenseUpdateAssign");
 
 extern "C" {
   int op_DenseUpdateAssign(const void* arg, size_t len);
+  int op_DenseUpdateAssignAdd(const void* arg, size_t len);
+  int op_DenseUpdateAssignSub(const void* arg, size_t len);
 }
-
 
 //
 // DenseUpdateAssign
@@ -65,6 +68,113 @@ int op_DenseUpdateAssign(const void* args, size_t len)
   else if (p->dtype == DT_DOUBLE) {
     ret = dense_update_assign<double>(p->num_elements,
                                       p->dst_ptr, p->src_ptr) ;
+  } else if (p->dtype == DT_INT64) {
+    ret = dense_update_assign<int64_t>(p->num_elements,
+                                       p->dst_ptr, p->src_ptr) ;
+  }
+
+  LOG(2) << __FUNCTION__ << " end. ret=" << ret;
+  return ret;
+}
+
+//
+// DenseUpdateAssignAdd
+//
+
+namespace {
+
+template <typename T>
+int dense_update_assign_add(int64_t num_elements,
+                    uint64_t dst_ptr, uint64_t src_ptr )
+{
+  T* dst = reinterpret_cast<T*>(dst_ptr);
+  const T* src = reinterpret_cast<const T*>(src_ptr);
+
+#pragma omp parallel for
+  for(int64_t i=0; i<num_elements; i++) {
+    dst[i] += src[i] ;
+  }
+  return 0 ;
+}
+}
+
+int op_DenseUpdateAssignAdd(const void* args, size_t len)
+{
+  LOG(2) << __FUNCTION__ << " begin";
+
+  struct Args {
+    int dtype;
+    int64_t num_elements ;
+    uint64_t dst_ptr, src_ptr ;
+  } const* p;
+
+  CHECK_ARG_LEN(len, sizeof(Args));
+  p = reinterpret_cast<const Args*>(args);
+
+  int ret = 1;
+
+  if (p->dtype == DT_FLOAT) {
+    ret = dense_update_assign_add<float> (p->num_elements,
+                                          p->dst_ptr, p->src_ptr) ;
+  }
+  else if (p->dtype == DT_DOUBLE) {
+    ret = dense_update_assign_add<double>(p->num_elements,
+                                          p->dst_ptr, p->src_ptr) ;
+  } else if (p->dtype == DT_INT64) {
+    ret = dense_update_assign_add<int64_t>(p->num_elements,
+                                           p->dst_ptr, p->src_ptr) ;
+  }
+
+  LOG(2) << __FUNCTION__ << " end. ret=" << ret;
+  return ret;
+}
+
+//
+// DenseUpdateAssignSub
+//
+
+namespace {
+
+template <typename T>
+int dense_update_assign_sub(int64_t num_elements,
+                    uint64_t dst_ptr, uint64_t src_ptr )
+{
+  T* dst = reinterpret_cast<T*>(dst_ptr);
+  const T* src = reinterpret_cast<const T*>(src_ptr);
+
+#pragma omp parallel for
+  for(int64_t i=0; i<num_elements; i++) {
+    dst[i] -= src[i] ;
+  }
+  return 0 ;
+}
+}
+
+int op_DenseUpdateAssignSub(const void* args, size_t len)
+{
+  LOG(2) << __FUNCTION__ << " begin";
+
+  struct Args {
+    int dtype;
+    int64_t num_elements ;
+    uint64_t dst_ptr, src_ptr ;
+  } const* p;
+
+  CHECK_ARG_LEN(len, sizeof(Args));
+  p = reinterpret_cast<const Args*>(args);
+
+  int ret = 1;
+
+  if (p->dtype == DT_FLOAT) {
+    ret = dense_update_assign_sub<float> (p->num_elements,
+                                          p->dst_ptr, p->src_ptr) ;
+  }
+  else if (p->dtype == DT_DOUBLE) {
+    ret = dense_update_assign_sub<double>(p->num_elements,
+                                          p->dst_ptr, p->src_ptr) ;
+  } else if (p->dtype == DT_INT64) {
+    ret = dense_update_assign_sub<int64_t>(p->num_elements,
+                                           p->dst_ptr, p->src_ptr) ;
   }
 
   LOG(2) << __FUNCTION__ << " end. ret=" << ret;
