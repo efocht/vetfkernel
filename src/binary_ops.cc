edd889cacdbf8e5f01b02f07770c4d671ea39401
Yoshiyuki Ohno <y-ohno@ji.jp.nec.com>
2019-01-17 20:16:51 +0900

    add Equal_op

--- src/binary_ops.cc
+++ src/binary_ops.cc
@@ -34,6 +34,7 @@ REGISTER_KERNEL("Div", "op_Div");
 REGISTER_KERNEL("DivNoNan", "op_DivNoNan");
 REGISTER_KERNEL("Minimum", "op_Minimum");
 REGISTER_KERNEL("Maximum", "op_Maximum");
+REGISTER_KERNEL("Equal", "op_Equal");
 REGISTER_KERNEL("LessEqual", "op_LessEqual");
 REGISTER_KERNEL("GreaterEqual", "op_GreaterEqual");
 
@@ -45,6 +46,7 @@ extern "C" {
   int op_DivNoNan(const void* arg, size_t len);
   int op_Minimum(const void* arg, size_t len);
   int op_Maximum(const void* arg, size_t len);
+  int op_Equal(const void* arg, size_t len);
   int op_LessEqual(const void* arg, size_t len);
   int op_GreaterEqual(const void* arg, size_t len);
 }
@@ -433,6 +435,68 @@ int op_mul(const BinaryOpArgs& args) {
   return 1;
 }
 
+// Equal
+
+template <typename T>
+int equal_n1(uint64_t out, uint64_t in0, uint64_t in1, size_t n)
+{
+  bool* po = reinterpret_cast<bool*>(out);
+  const T* pi0 = reinterpret_cast<const T*>(in0);
+  T i1 = *reinterpret_cast<const T*>(in1);
+
+  for (size_t i = 0; i < n; ++i) {
+    po[i] = (pi0[i] == i1);
+  }
+  return 0;
+}
+
+template <typename T>
+int equal_nn(uint64_t out, uint64_t in0, uint64_t in1, size_t n)
+{
+  bool* po = reinterpret_cast<bool*>(out);
+  const T* pi0 = reinterpret_cast<const T*>(in0);
+  const T* pi1 = reinterpret_cast<const T*>(in1);
+
+  for (size_t i = 0; i < n; ++i) {
+    po[i] = (pi0[i] == pi1[i]);
+  }
+  return 0;
+}
+
+int op_equal(const BinaryOpArgs& args) {
+  if (CheckTypes(args, DT_FLOAT, DT_FLOAT, DT_BOOL)) {
+    if (args.in1.nelems == 1) {
+      return equal_n1<float>(args.out.addr, args.in0.addr, args.in1.addr,
+                             args.in0.nelems);
+    }
+    else if( args.in0.nelems == args.in1.nelems ) {
+      return equal_nn<float>(args.out.addr, args.in0.addr, args.in1.addr,
+                              args.in0.nelems);
+    }
+  }
+  else if (CheckTypes(args, DT_DOUBLE, DT_DOUBLE, DT_BOOL)) {
+    if (args.in1.nelems == 1) {
+      return equal_n1<double>(args.out.addr, args.in0.addr, args.in1.addr,
+                              args.in0.nelems);
+    }
+    else if( args.in0.nelems == args.in1.nelems ) {
+      return equal_nn<double>(args.out.addr, args.in0.addr, args.in1.addr,
+                              args.in0.nelems);
+    }
+  }
+  else if (CheckTypes(args, DT_INT64, DT_INT64, DT_BOOL)) {
+    if (args.in1.nelems == 1) {
+      return equal_n1<int64_t>(args.out.addr, args.in0.addr, args.in1.addr,
+                              args.in0.nelems);
+    }
+    else if( args.in0.nelems == args.in1.nelems ) {
+      return equal_nn<int64_t>(args.out.addr, args.in0.addr, args.in1.addr,
+                              args.in0.nelems);
+    }
+  }
+  
+  return 1;
+}
 
 // LessEqual
 
@@ -770,7 +834,10 @@ int op_Maximum(const void* args, size_t len)
 {
   return op_Binary(args, len, op_maximum, "op_Maximum");
 }
-
+int op_Equal(const void* args, size_t len)
+{
+  return op_Binary(args, len, op_equal, "op_Equal");
+}
 int op_LessEqual(const void* args, size_t len)
 {
   return op_Binary(args, len, op_lessEqual, "op_LessEqual");
